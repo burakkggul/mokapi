882b6b149b9298ab1fbeb28f4b6c345ad2b47bac
Burak GUL <burak.gul@ozguryazilim.com.tr>
2020-12-31 14:42:22 +0300

    initial commit

---
+++ src/main/java/tr/com/burakgul/mokapi/MokapiApplication.java
@@ -0,0 +1,13 @@
+package tr.com.burakgul.mokapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/com/burakgul/mokapi/MokapiApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.burakgul.mokapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
