687a8748a8a10ba5b566ca6706bb00c58cdbe2cc
Burak GUL <burakkggul@gmail.com>
2021-02-05 21:14:42 +0300

    swagger redirect controller name change

--- src/main/java/tr/com/burakgul/mokapi/controller/SwaggerRedirectController.java
+++ src/main/java/tr/com/burakgul/mokapi/controller/SwaggerRedirectController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class m SwaggerRedirectController {
+public class SwaggerRedirectController {
 
     @RequestMapping("/")
     public String redirectSwagger() {
